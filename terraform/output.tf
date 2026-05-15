83ece0e0a96ab78ba8b6d0db190be06f6fef4b4c
Thu-sunrise <thu.hominh1704xx@hcmut.edu.vn>
2026-05-16 01:59:14 +0700

    add config s3

--- terraform/output.tf
+++ terraform/output.tf
@@ -9,3 +9,8 @@ output "Monitor_Public_IP" {
   description = "Link truy cập Grafana"
   value       = "http://${aws_instance.monitor_server.public_ip}:3000"
 }
+
+output "Frontend_S3_URL" {
+  description = "Link truy cập Frontend trên S3"
+  value       = "http://${aws_s3_bucket_website_configuration.frontend_website.website_endpoint}"
+}
