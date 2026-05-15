2d3b416e3db43f4d7fbe101e0630bbb6c5cd2656
Thu-sunrise <thu.hominh1704xx@hcmut.edu.vn>
2026-05-16 01:15:49 +0700

    tach fe

--- client/src/api/customerDashboardApi.js
+++
@@ -1,8 +0,0 @@
-import axiosClient from "./axiosClient";
-
-export const customerApi = {
-  // Get top 10 selling customers
-  getTopSelling: () => axiosClient.get("/customer/top-selling"),
-};
-
-export default customerApi;
